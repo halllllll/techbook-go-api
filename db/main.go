11ec28efcfa4be4d024e0b61ed2b5196c122cc56
h4lllllll <hjod1172@yahoo.co.jp>
2023-11-29 08:02:30 +0900

    トランザクション(1)

--- db/main.go
+++ db/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/halllllll/techbook-go-api/db/models"
 )
 
 func main() {
@@ -43,25 +42,72 @@ func main() {
 	// 	article.CreatedAt = createdTime.Time
 	// }
 
-	article := models.Article{
-		Title:    "insert test ONE",
-		Contents: "Can I Insert Data Correctly?",
-		UserName: "saki-ikas",
-	}
-	const sqlStr = `
-		INSERT INTO articles (
-			title, contents, username, nice, created_at
-		) VALUES (
-			?, ?, ?, 0, now()
-		);
-	`
+	// article := models.Article{
+	// 	Title:    "insert test ONE",
+	// 	Contents: "Can I Insert Data Correctly?",
+	// 	UserName: "saki-ikas",
+	// }
+	// const sqlStr = `
+	// 	INSERT INTO articles (
+	// 		title, contents, username, nice, created_at
+	// 	) VALUES (
+	// 		?, ?, ?, 0, now()
+	// 	);
+	// `
 
-	result, err := db.Exec(sqlStr, article.Title, article.Contents, article.UserName)
+	// result, err := db.Exec(sqlStr, article.Title, article.Contents, article.UserName)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	// fmt.Println(result.LastInsertId())
+	// fmt.Println(result.RowsAffected())
+
+	// トランザクション
+	tx, err := db.Begin()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	article_id := 1
+	const sqlGetNice = `
+		SELECT nice
+		FROM articles
+		WHERE article_id = ?
+	`
 
-	fmt.Println(result.LastInsertId())
-	fmt.Println(result.RowsAffected())
+	row := tx.QueryRow(sqlGetNice, article_id)
+	if err := row.Err(); err != nil {
+		// ロールバックを忘れずに
+		fmt.Println(err)
+		tx.Rollback()
+		return
+	}
+
+	var nicenum int
+	err = row.Scan(&nicenum)
+	if err != nil {
+		// ロールバックを忘れずに
+		fmt.Println(err)
+		tx.Rollback()
+		return
+	}
+
+	const sqlUpdateNice = `
+		UPDATE articles
+		SET nice = ?
+		WHERE article_id = ?
+	`
+	_, err = tx.Exec(sqlUpdateNice, nicenum+1, article_id)
+	if err != nil {
+		// ロールバックを忘れずに
+		fmt.Println(err)
+		tx.Rollback()
+		return
+	}
+
+	// コミットを忘れずに
+	tx.Commit()
+
 }

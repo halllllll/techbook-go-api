55bf1fbe8ac668689ff6c24f577d5e3d87f11477
halllllll <h4l>
2023-11-29 14:21:15 +0900

    db, api, test(1)

--- db/main.go
+++ db/main.go
@@ -20,50 +20,6 @@ func main() {
 
 	defer db.Close()
 
-	// articleID := 1
-
-	// const sqlStr = `
-	// 	SELECT * FROM articles WHERE article_id = ?;
-	// `
-	// row := db.QueryRow(sqlStr, articleID)
-	// if err := row.Err(); err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-	// var article models.Article
-	// var createdTime sql.NullTime
-
-	// err = row.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-	// if createdTime.Valid {
-	// 	article.CreatedAt = createdTime.Time
-	// }
-
-	// article := models.Article{
-	// 	Title:    "insert test ONE",
-	// 	Contents: "Can I Insert Data Correctly?",
-	// 	UserName: "saki-ikas",
-	// }
-	// const sqlStr = `
-	// 	INSERT INTO articles (
-	// 		title, contents, username, nice, created_at
-	// 	) VALUES (
-	// 		?, ?, ?, 0, now()
-	// 	);
-	// `
-
-	// result, err := db.Exec(sqlStr, article.Title, article.Contents, article.UserName)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-
-	// fmt.Println(result.LastInsertId())
-	// fmt.Println(result.RowsAffected())
-
 	// トランザクション
 	tx, err := db.Begin()
 	if err != nil {
---
+++ server/repositories/article_test.go
@@ -0,0 +1,58 @@
+package repositories_test
+
+import (
+	"database/sql"
+	"fmt"
+	"testing"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/halllllll/techbook-go-api/server/models"
+	"github.com/halllllll/techbook-go-api/server/repositories"
+)
+
+func TestSelectArticleDetail(t *testing.T) {
+	dbUser := "docker"
+	dbPassword := "docker"
+	dbDatabase := "sampledb"
+	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer db.Close()
+
+	expected := models.Article{
+		ID:       1,
+		Title:    "firstPoost",
+		Contents: "This is my first blog",
+		UserName: "saki",
+		NiceNum:  3,
+	}
+
+	got, err := repositories.SelectArticleDetail(db, expected.ID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if got.ID != expected.ID {
+		t.Errorf("get %d but want %d\n", got.ID, expected.ID)
+	}
+
+	if got.Title != expected.Title {
+		t.Errorf("get %s but want %s\n", got.Title, expected.Title)
+	}
+
+	if got.Contents != expected.Contents {
+		t.Errorf("get %s but want %s\n", got.Contents, expected.Contents)
+	}
+	if got.UserName != expected.UserName {
+		t.Errorf("get %s but want %s\n", got.UserName, expected.UserName)
+	}
+	if got.NiceNum != expected.NiceNum {
+		t.Errorf("get %d but want %d\n", got.NiceNum, expected.NiceNum)
+	}
+
+}

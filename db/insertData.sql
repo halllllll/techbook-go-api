aecdbb4ec5275475f456e3cf26f6c84d08a9f355
h4lllllll <hjod1172@yahoo.co.jp>
2023-11-29 21:01:51 +0900

    タイポと環境違いもろもろでテストが通らなかったのをエイッってやる

--- db/insertData.sql
+++ db/insertData.sql
@@ -1,9 +1,9 @@
 INSERT INTO articles (title, contents, username, nice, created_at) VALUES (
-    'firstPoost', 'This is my first blog', 'saki', 2, now()
+    'firstPost', 'This is my first blog', 'saki', 4, now()
 );
 
 INSERT INTO articles (title, contents, username, nice) VALUES (
-    '2nd Post', 'Second Blog Post', 'saki', 4
+    '2nd Post', 'Second Blog Post', 'saki', 9
 );
 
 INSERT INTO comments(article_id, message, created_at) VALUES(1, '1st comment yeah', now());
